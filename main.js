05d2e4ac4b8eee849b34d6d59eafae2031da187a
Pedro Schneider <correaschneider@gmail.com>
2019-08-17 17:28:21 -0300

    Object Short Syntax

---
+++ main.js
@@ -0,0 +1,16 @@
+const name = "Pedro"
+const age = 34
+
+// const user = {
+//     name: name,
+//     age: age,
+//     company: "CWI"
+// }
+
+const user = {
+    name,
+    age,
+    company: "CWI"
+}
+
+console.log(user)
