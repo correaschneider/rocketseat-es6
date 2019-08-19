18bf36162ed363a73d03e4212016701dd85d1e76
Pedro Schneider <pedro.schneider@cwi.com.br>
2019-08-19 12:45:41 -0300

    Estrutura e estilo

--- src/functions.js
+++
@@ -1,7 +0,0 @@
-export const age = 34;
-
-export default class User {
-    static info() {
-        console.log('Apenas teste');
-    }
-}
--- src/main.js
+++
@@ -1,5 +0,0 @@
-import ClassUser, { age as UserAge } from './functions'
-
-ClassUser.info()
-
-document.body.append(UserAge)
